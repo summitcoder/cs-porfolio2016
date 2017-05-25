834b8e88002d17b4db2dd4819c9aa25729c9933e
Isaac Bojorquez <ibojorquez.sp@mysummitps.org>
2017-05-25 18:36:45 +0000

    update

--- Website.js
+++ Website.js
@@ -1,3 +1,8 @@
+// here you put difine the images and the text that you are gonna use in the website
+// You use the var tag and getElementById to difine them. I put Img next to the images
+// so I wouldn't get them confused when I was adding the event listeners. I started the
+// var with the information with hitlerinfo so I known that anything below that is info.
+
 var hitlerImg = document.getElementById("hitlerImg")
 var naziImg = document.getElementById("naziImg")
 var sovietImg = document.getElementById("sovietImg")
@@ -7,7 +12,13 @@ var hitlerinfo = document.getElementById("hitlerinfo")
 var naziterritory = document.getElementById("naziterritory")
 var battleofbritain = document.getElementById("battleofbritain")
 var sovietinvasion = document.getElementById("sovietinvasion")
-var ddaylanding = document.getElementById("dday")
+var ddaylanding = document.getElementById("ddaylanding")
+
+// This is where we add the event listener so when we click the image the text pops up.
+// under the event listener I added an if else tag so the text will pop up or go away 
+// only when I clicked the image. I used a .style.dysplay so it knows that I am trying 
+// to display text. I added an event listener for every single image so the text can pop 
+// up or go away by clicking each individual photo. 
 
 hitlerImg.addEventListener("click", function() {
 
